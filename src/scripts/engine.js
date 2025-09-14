8d753385ed0b59f6443de898b5221597771dbafe
Matheus Rocha <matheusrochaalmeida7@gmail.com>
2025-09-14 00:42:09 -0300

    Update engine.js

--- src/scripts/engine.js
+++ src/scripts/engine.js
@@ -8,7 +8,7 @@ const state = {
     },
     values:{
         timerId:null,
-        intervaloVelocidadeJogo: 1000,
+        intervaloVelocidadeJogo: 500,
         posicao: 0,
         result: 0,
         tempo: 60,
